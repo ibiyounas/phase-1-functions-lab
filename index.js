0d011559a59857995ec0768b09aadf92d4f23da8
Ibrahem Younas <younasibrahem@gmail.com>
2024-04-02 12:21:45 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return street > 42 ?  street - 42 : 42 - street;
+
+    //returns the number of blocks given a value
+  }
+  function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street)*264;
+  }
+  function distanceTravelledInFeet(start, end) {
+    return end > start? (end - start) * 264 : (start - end) * 264;
+
+  }
+  function calculatesFarePrice(start, end) {
+    //returns the fare for the customer
+  
+    if (distanceTravelledInFeet(start, end) <= 400) {
+        return 0;
+    } 
+    else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) <= 2000) {
+        return .02 * (distanceTravelledInFeet(start, end) - 400);
+    }
+    else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) <= 2500) {
+        return 25;
+
+    }
+    else if (distanceTravelledInFeet(start, end) > 2500) {
+        return "cannot travel that far";
+    }
+  
+}
